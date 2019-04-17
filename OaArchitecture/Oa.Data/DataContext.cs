cc6bcf191862c6e8abaee6f58a26f2415a166643
Abdelkhalek BEMBARKA <A.BEMBARKA@gmail.com>
2019-04-18 01:30:53 +0200

    Data  and initial Repo

---
+++ OaArchitecture/Oa.Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Oa.Data
+{
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Oa.Data.EntitiesMap;
+
+    public partial class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserProfile> UserProfile { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=beserver;Database=OaArche;User Id=sa;Password=0101;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<User>(entity => {new UserMap(entity); });
+
+            modelBuilder.Entity<UserProfile>(entity => {new UserProfileMap(entity); });
+        }
+    }
+}
